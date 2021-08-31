80faf7ce44479851550e4f37ad6a37cfffff288d
LaraSakanaka <85855158+LaraYuka@users.noreply.github.com>
2021-08-31 15:38:47 +0000

    Initial commit

---
+++ js/fundo.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+
+   $('.btn-original').click(function(original){
+    original.preventDefault()
+
+    $('body').removeClass('claro')
+    $('body').removeClass('escuro')
+    $('body').addClass('original')
+    $('p').removeClass('text-dark')
+    $('p').addClass('text-white')
+  })
+
+  $('.btn-escuro').click(function(escuro){
+    escuro.preventDefault()
+
+    $('body').removeClass('original')
+    $('body').removeClass('claro')
+    $('body').addClass('escuro')
+    $('p').removeClass('text-dark')
+    $('p').addClass('text-white')
+  })
+
+  $('.btn-claro').click(function(claro){
+    claro.preventDefault()
+
+    $('body').removeClass('original')
+    $('body').removeClass('escuro')
+    $('body').addClass('claro')
+    $('p').removeClass('text-whit')
+    $('p').addClass('text-dark')
+  })
+
+})
